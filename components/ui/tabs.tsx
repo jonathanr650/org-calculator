3fd779f5aa21ac7891b4b172915698fc67fd9310
Isaac Ramirez <168313933+jonathanr650@users.noreply.github.com>
2025-01-30 10:28:17 -0800

    Update tabs.tsx

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,8 +1,9 @@
 "use client";
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
 
-const Tabs = TabsPrimitive.Root
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+
+const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -13,8 +14,8 @@ const TabsList = React.forwardRef
     className="inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground"
     {...props}
   />
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.Trigger>,
@@ -25,8 +26,8 @@ const TabsTrigger = React.forwardRef
     className="inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
     {...props}
   />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.Content>,
@@ -37,7 +38,7 @@ const TabsContent = React.forwardRef
     className="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
     {...props}
   />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
